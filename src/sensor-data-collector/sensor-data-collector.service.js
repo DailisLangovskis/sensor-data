edf8cae6f98ab323039cea0f831513319a48ed43
DailisLangovskis <dailis.langovskis@gmail.com>
2019-11-05 19:39:02 +0200

    -added sensor data collector to sidebar

--- src/sensor-data-collector/sensor-data-collector.service.js
+++ src/sensor-data-collector/sensor-data-collector.service.js
@@ -1,7 +1,7 @@
 export default ['Core', 'hs.utils.service', '$rootScope', '$http',
     function (Core, utils, $rootScope, $http) {
         var me = {
-            
+
         };
         return me;
     }
